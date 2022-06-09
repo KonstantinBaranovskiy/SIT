72813fec49d041eec094016bef7c01ce019da3c5
Konstantin Baranovskiy <baranovskij28105@gmail.com>
2022-06-09 23:12:17 +0300

    Added fib function

--- myproject.cpp
+++ myproject.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+	int fib(int n) {
+		if (n <= 2)
+			return 1;
+		return fib(n - 1) + fib(n - 2);
+	}
+
+	int main() {
+		std::cout << "Hello World!\n";
+		std::cout << fib(10);
+		return 0;
+	}
